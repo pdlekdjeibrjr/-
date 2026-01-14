4b0c855f87d34b3aeaf1b6165dbb9c279674ca11
pdlekdjeibrjr <darinasargina78@gmail.com>
2026-01-14 21:51:27 +0300

    Add files via upload

---
+++ 1.14.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cout << "Введите три числа: ";
+    cin >> a >> b >> c ;
+    cout << a << " " << b << " " << c  << endl;
+    return 0;
+}
